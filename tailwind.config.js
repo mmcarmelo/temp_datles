b5afb3d58ce5876ba69db758c999ebd76edcb901
luis carmelo <luiscarmelo@luiss-MBP.home>
2020-07-01 14:36:19 +0100

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,68 @@
 module.exports = {
-  purge: [],
   theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },
+    borderRadius: {
+      'none': '0',
+      'sm': '.125rem',
+      default: '.25rem',
+      'lg': '.5rem',
+      'xl': '1rem',
+      'full': '9999px',
+    },
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        cyan: '#9cdbff',
+        login:  '#13131A',
+        bluebtn: '#1E75FF',
+        dark: '#1C1C24',
+        green: '#3FC75E',
+      },
+      spacing: {
+        px: '1px',
+        '0': '0',
+        '1': '0.25rem',
+        '2': '0.5rem',
+        '3': '0.75rem',
+        '4': '1rem',
+        '5': '1.25rem',
+        '6': '1.5rem',
+        '8': '2rem',
+        '10': '2.5rem',
+        '12': '3rem',
+        '16': '4rem',
+        '20': '5rem',
+        '24': '6rem',
+        '32': '8rem',
+        '40': '10rem',
+        '48': '12rem',
+        '56': '14rem',
+        '64': '16rem',
+      },
+      opacity: {
+        '0': '0',
+        '20': '0.2',
+        '40': '0.4',
+        '60': '0.6',
+        '80': '0.8',
+        '100': '1',
+      },
+      
+    }
+  }
 }
